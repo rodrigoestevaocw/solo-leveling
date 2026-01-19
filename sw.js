29f6ebc618c115367985ff72b185afd69b46d89f
rodrigoestevaocw <rodrigoestevaocw@gmail.com>
2026-01-19 22:58:39 -0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "system-cache-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
